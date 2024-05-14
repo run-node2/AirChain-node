c5216f9719008796ad06ef0e567447670cc6b38c
run-node2 <pinkskies0926@gmail.com>
2024-05-14 14:36:27 +0800

    Update Delegate.sh

--- Delegate.sh
+++ Delegate.sh
@@ -59,7 +59,7 @@ function delegate_staking() {
     sed -i "s|\$validator|$air_validator|g" air.sh
 
     # 获取port并替换 air.sh 中的占位符
-    junctiond_RPC_PORT=$(grep -E '^export junctiond_RPC_PORT=' ./bash_profile | cut -d= -f2-)
+    junctiond_RPC_PORT=$(grep -E '^export junctiond_RPC_PORT=' /.bash_profile | cut -d= -f2-)
     sed -i "s|\$junctiond_RPC_PORT|$junctiond_RPC_PORT|g" air.sh
 
 
