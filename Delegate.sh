ce761f86b4b12e22609062d6d672231d1b4a916d
run-node2 <pinkskies0926@gmail.com>
2024-05-18 23:03:05 +0800

    Update Delegate.sh

--- Delegate.sh
+++ Delegate.sh
@@ -167,9 +167,8 @@ screen -r delegate_airchain
 # 主菜单
 function main_menu() {
   clear
-  echo "=====================专用脚本 盗者必究==========================="
+  echo "============================自用脚本============================="
   echo "需要测试网节点部署托管 技术指导 部署领水质押脚本 请联系Telegram :https://t.me/linzeusasa"
-  echo "需要测试网节点部署托管 技术指导 部署领水质押脚本 请联系Wechat :llkkxx001"
   echo "1. 安装基础环境"
   echo "2. 查询AirChain钱包信息"
   echo "3. 配置AirChain节点信息"
